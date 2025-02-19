4e796c0e51b032d4711626795256e4542be166cb
Ian Macartney <ianhmacartney@gmail.com>
2025-02-19 01:23:37 -0800

    any

--- src/component/lib.ts
+++ src/component/lib.ts
@@ -364,8 +364,7 @@ async function runOnComplete(
   logger: Logger,
   ctx: MutationCtx,
   run: Doc<"committed">,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  result: any
+  result: RunResult
 ) {
   if (!run.options.onComplete) {
     return;
@@ -374,10 +373,7 @@ async function runOnComplete(
     logger.debug(`Running onComplete handler for ${run._id}`);
     const handle = run.options.onComplete as FunctionHandle<
       "mutation",
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      { runId: Id<"incoming">; context: any; result: RunResult },
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      any
+      { runId: Id<"incoming">; context: unknown; result: RunResult }
     >;
     await ctx.runMutation(handle, {
       runId: run.incomingId,
